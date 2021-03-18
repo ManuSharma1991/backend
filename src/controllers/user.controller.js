b8550b07279aa8f6cd345e3023bd49a656833322
Manu Sharma <manu.viswanad@gmail.com>
2021-03-18 15:37:32 +0530

    issue 1

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,6 +5,7 @@ const Category = require('../db/models/category.model');
 const SubCategory = require('../db/models/subCategory.model');
 const Transaction = require('../db/models/transaction.model');
 const User = require('../db/models/user.model');
+const Promise = require('bluebird');
 
 
 getUser = async (req, res, next) => {
@@ -21,7 +22,8 @@ getUser = async (req, res, next) => {
 
 createUser = async (req, res, next) => {
     try {
-        const user = await User.save(req.body);
+        const newUser = new User(req.body);
+        const user = await newUser.save();
         res.send(user);
     } catch (err) {
         res.status(500).send({
@@ -50,18 +52,29 @@ populateUserData = async (user) => {
     const accounts = await Account.find({ 'user': user._id }, { _id: 0, __v: 0, createdAt: 0, updatedAt: 0, user: 0 });
     user.account = accounts;
 
-
     const categories = await Category.find({ 'user': user._id }, { _id: 0, __v: 0, createdAt: 0, updatedAt: 0, user: 0 });
     user.category = categories;
 
     const subCategories = await SubCategory.find({ 'user': user._id }, { _id: 0, __v: 0, createdAt: 0, updatedAt: 0, user: 0 }).populate('category');
     user.subCategory = subCategories;
 
-    const allocations = await Allocation.find({ 'user': user._id }, { _id: 0, __v: 0, createdAt: 0, updatedAt: 0, user: 0 });
-    user.allocation = allocations;
+    const allocations = await Allocation.find({ 'user': user._id }, { _id: 0, __v: 0, createdAt: 0, updatedAt: 0, user: 0 }).populate('category').populate('subCategory');
+    const modified_allocations = allocations.map(getBudgetId)
+    user.allocation = modified_allocations;
+    console.log('1')
 
     const transactions = await Transaction.find({ 'user': user._id }, { _id: 0, __v: 0, createdAt: 0, updatedAt: 0, user: 0 });
     user.transaction = transactions;
+}
+
+function getBudgetId(allocation) {
+    Budget.findById(allocation.budget)
+        .then(budget => {
+            console.log(budget);
+            allocation.budget = budget.budgetId;
+        })
+    console.log('2')
+    return allocation
 }
 
 module.exports = { getUser, createUser, getUserById }
